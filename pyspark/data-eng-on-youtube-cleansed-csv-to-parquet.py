2abcf450713976c4c2a8ce8f25917e4ecc3e444f
Pranav Jha <pranav.jha@mail.concordia.ca>
2024-07-25 23:29:20 -0400

    Synchronizing job data-eng-on-youtube-cleansed-csv-to-parquet from AWS Glue at 2024/07/26 03:29:20

--- pyspark/data-eng-on-youtube-cleansed-csv-to-parquet.py
+++ pyspark/data-eng-on-youtube-cleansed-csv-to-parquet.py
@@ -18,8 +18,10 @@ predicate_pushdown = "region = 'ca' OR region = 'gb' OR region = 'us'"
 # Script generated for node Amazon S3
 AmazonS3_node1721959861815 = glueContext.create_dynamic_frame.from_options(format_options={"quoteChar": "\"", "withHeader": True, "separator": ","}, connection_type="s3", format="csv", connection_options={"paths": ["s3://data-eng-on-youtube-raw-us-east-1-dev/youtube/raw_statistics/"], "recurse": True}, transformation_ctx="AmazonS3_node1721959861815", push_down_predicate = predicate_pushdown)
 
+AmazonS3_node1721959861816 = AmazonS3_node1721959861815.resolveChoice(specs=[("region", "cast:string")])
+
 
 # Script generated for node Amazon S3
-AmazonS3_node1721959904716 = glueContext.write_dynamic_frame.from_options(frame=AmazonS3_node1721959861815, connection_type="s3", format="glueparquet", connection_options={"path": "s3://data-eng-on-youtube-cleansed-us-east-1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1721959904716")
+AmazonS3_node1721959904716 = glueContext.write_dynamic_frame.from_options(frame=AmazonS3_node1721959861816, connection_type="s3", format="glueparquet", connection_options={"path": "s3://data-eng-on-youtube-cleansed-us-east-1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1721959904716")
 
 job.commit()
