9662e352d8f764b41ebe35e9ef9f59b7f20c0080
Pranav Jha <pranav.jha@mail.concordia.ca>
2024-07-25 23:15:11 -0400

    Synchronizing job data-eng-on-youtube-cleansed-csv-to-parquet from AWS Glue at 2024/07/26 03:15:11

---
+++ pyspark/data-eng-on-youtube-cleansed-csv-to-parquet.py
@@ -0,0 +1,33 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME'])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args['JOB_NAME'], args)
+
+predicate_pushdown = "region in ('ca','gb','us')"
+
+# Script generated for node Amazon S3
+
+AmazonS3_node1721959861815 = glueContext.create_dynamic_frame.from_catalog(format_options={"quoteChar": "\"", "withHeader": True, "separator": ","}, connection_type="s3", format="csv", connection_options={"paths": ["s3://data-eng-on-youtube-raw-us-east-1-dev/youtube/raw_statistics/"], "recurse": True}, transformation_ctx="AmazonS3_node1721959861815", push_down_predicate = predicate_pushdown)
+
+
+dropnullfields3 = DropNullFields.apply(frame = AmazonS3_node1721959861815, transformation_ctx = "dropnullfields3")
+
+
+datasink1 = dropnullfields3.toDF().coalesce(1)
+df_final_output = DynamicFrame.fromDF(datasink1, glueContext, "df_final_output")
+
+# Script generated for node Amazon S3
+AmazonS3_node1721959904716 = glueContext.write_dynamic_frame.from_options(frame=df_final_output, connection_type="s3", format="glueparquet", connection_options={"path": "s3://data-eng-on-youtube-cleansed-us-east-1-dev/youtube/raw_statistics/", "partitionKeys": ["region"]}, format_options={"compression": "snappy"}, transformation_ctx="AmazonS3_node1721959904716")
+
+
+job.commit()
